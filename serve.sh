9c0619b2214fc5e170f612258eef8e677b4d7091
Herbert Ullrich <ullriher@fel.cvut.cz>
2020-07-08 13:43:57 +0200

    serve.sh generalisation

--- serve.sh
+++ serve.sh
@@ -1,1 +1,2 @@
-/home/bertik/miniconda3/envs/fever-cs-baseline/bin/waitress-serve --host=0.0.0.0  --port=5000 --call fever_cs:make_api
+#pip install -r requirements.txt
+waitress-serve --host=0.0.0.0  --port=5000 --call fever_cs:make_api
